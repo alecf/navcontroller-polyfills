67975f4804234072e7ab9425b5bd0287ae3d0559
Alec Flett <alecflett@chromium.org>
2013-08-23 17:54:52 -0700

    add url script for resolving relative urls without the dom, add cat substitution.

--- demo/testcontroller.js
+++ demo/testcontroller.js
@@ -3,5 +3,12 @@ LOG = [];
 
 onfetch = function(event) {
     LOG.push(["Fetch event: ", event]);
-
+    var cache = new Cache("cats",
+                          "creepy-cat-1.jpeg",
+                          "creepy-cat-2.jpeg");
+    if (/cat.*jpeg$/.exec(event.request.url)) {
+        event.request.url = event.request.url.replace("cute", "creepy");
+        LOG.push("Found cute cat, replacing with ", event.request.url);
+        event.respondWith(cache.match(event.request));
+    }
 };
