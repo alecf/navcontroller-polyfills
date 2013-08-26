32168cd3a1d2208fda752de80607fa9a8096f81f
Alec Flett <alecflett@chromium.org>
2013-08-26 12:44:04 -0700

    update demo to account for console.log

--- demo/testcontroller.js
+++ demo/testcontroller.js
@@ -1,14 +1,17 @@
 
-LOG = [];
-
 onfetch = function(event) {
-    LOG.push(["Fetch event: ", event]);
+    console.log("Fetch event: ", event);
     var cache = new Cache("cats",
                           "creepy-cat-1.jpeg",
                           "creepy-cat-2.jpeg");
     if (/cat.*jpeg$/.exec(event.request.url)) {
         event.request.url = event.request.url.replace("cute", "creepy");
-        LOG.push("Found cute cat, replacing with ", event.request.url);
-        event.respondWith(cache.match(event.request));
+        console.log("Found cute cat, replacing with ", event.request.url);
+        var response = cache.match(event.request);
+        response.then(function(v) {
+            console.log("From cache for ", event.request.url, ": ", v);
+            return v;
+        });
+        event.respondWith(response);
     }
 };
